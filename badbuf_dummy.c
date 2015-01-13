2fdb04f87ecadba208c577a46537435fc8e9521c
Shashank <agshash@seas.upenn.edu>
2015-01-13 16:27:29 -0500

    Create badbuf_dummy.c

---
+++ badbuf_dummy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int match(char *s1, char *s2)
+{
+  while( *s1 != '\0' && *s2 != '\0' && *s1 == *s2 ){
+  s1++; s2++;
+}
+return( *s1 - *s2 );
+}
+void welcome(char *str)
+{
+  printf(str);
+}
+void goodbye(char *str)
+{
+  void exit();
+  printf(str);
+  exit(1);
+}
+main()
+{
+  char name[128], pw[128]; /* passwords are short! */
+  char *good = "Welcome to The Machine!\n";
+  char *evil = "Invalid identity, exiting!\n";
+  printf("0x0000%lx\n",&name);
+  /*printf("login: "); //scanf("%s", name);
+  printf("password: ");// scanf("%s", pw);
+  if( match(name,pw) == 0 )
+  welcome( good );
+  else
+  goodbye(evil );*/
+  return 0;
+}
